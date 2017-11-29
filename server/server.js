21940c15e5d7fb41a74a51daab500c371c7ab565
thechamp262 <thefuturechamp@gmail.com>
2017-11-29 14:36:45 -0500

    Initial push for chat node  application

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.use(()=>{
+  res.render('index.html');
+})
+
+
+app.listen(port,()=>{
+  console.log(`Started on at port ${port}`);
+})
+
+// console.log(__dirname + "/../public");
+// console.log(publicPath);
