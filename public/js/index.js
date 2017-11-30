a521a16ed950fd3c8950c82e3168b1506f23d75a
thechamp262 <thefuturechamp@gmail.com>
2017-11-29 21:32:22 -0500

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+
+    let socket = io();
+    socket.on('connect',function(){
+      console.log("Connected to server");
+      socket.emit("createMessage",{
+        from: "melissa",
+        text: "This is some more dummy text!"
+      })
+    });
+
+    socket.on('disconnect',function(){
+      console.log("Disconnected from server!");
+    });
+
+    socket.on("newMessage",function(newMessage){
+      console.log("Incoming message",newMessage);
+    });
--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.use(()=>{
-//   res.render('index.html');
-// })
+io.on('connection',(socket)=>{
+  console.log('New users connected');
 
+  socket.on("disconnect",()=>{
+    console.log("The user has disconnected!");
+  })
+  //new message whofrom text createdattime stamp
+  socket.emit("newMessage",{
+    from: "test@email.com",
+    message: "This is some dummy text",
+    createdAT: 123321
+  });
+  socket.on("createMessage",(newMessage)=>{
+    console.log(`New Message from ${newMessage.from}.`, newMessage);
+  })
 
-app.listen(port,()=>{
+});
+
+server.listen(port,()=>{
   console.log(`Started on at port ${port}`);
 })
-
-// console.log(__dirname + "/../public");
-// console.log(publicPath);
